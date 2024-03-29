50b44a164ed3188175b151b759fb8f13c75367a3
Arnetta <arnetta.knight@gmail.com>
2024-03-29 16:37:39 -0400

    update auth

--- routes/auth.js
+++ routes/auth.js
@@ -6,3 +6,30 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+router.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: ' The user already exists'});
+        }
+
+        //Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Generate a two-step verification secret
+        const twoStepSecret = speakeasy.generateSecret();
+
+        //Create a new user
+        const newUser = new User ({
+            email,
+            password: hashedPassword,
+            twoStepSecret: twoStepSecret.base32
+        });
+
+        //Save the user to the database
+        await newUser.save();
+    }
+});
